6f33187ee2ddd71c25344002127a1542ad75bc85
Andy Cao <dongyicao123@gmail.com>
2021-11-12 10:02:28 -0800

    Starting code
    Starting Code for the chat app using swiftUI

---
+++ chatsApp/View/Authentication/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  chatsApp
+//
+//  Created by Andy Cao on 2021/11/11.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("registration view")
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
---
+++ chatsApp/chatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatsAppApp.swift
+//  chatsApp
+//
+//  Created by Andy Cao on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct chatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
